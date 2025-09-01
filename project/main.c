18ab8f7ebfd29170ab9b49f587ee815afc7d09a6
test11 <2932343667@qq.com>
2025-09-01 15:04:38 +0800

    第一次在vscode提交

--- project/main.c
+++ project/main.c
@@ -1,5 +1,11 @@
-#介绍这是一个测试项
+// 介绍这是一个测试项
 
- 我叫王福全 你叫什么 
- aaa  bbb  ccc
- xiugai 111 222
+//  我叫王福全 你叫什么
+//  aaa  bbb  ccc
+//  xiugai 111 222
+
+int main()
+{
+    printf("hello world\n");
+    return 0;
+}
